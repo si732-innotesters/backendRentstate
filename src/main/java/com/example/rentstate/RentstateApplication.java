752f9fd2b73193c9a1fd9733278697a9e8d7e684
Leydi-Aro <89089568+Leydi-Aro@users.noreply.github.com>
2023-11-02 17:08:49 -0500

    Initial commit

---
+++ src/main/java/com/example/rentstate/RentstateApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentstateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentstate/RentstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentstateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
