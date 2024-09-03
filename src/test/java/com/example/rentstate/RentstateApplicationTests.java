a410f7e9e53931a547d096f6e56e4acdad6d644f
GrandProgTest <u202124343@upc.edu.pe>
2024-09-03 11:41:50 -0500

    feat: added testing

--- src/test/java/com/example/rentstate/RentstateApplicationTests.java
+++ src/test/java/com/example/rentstate/RentstateApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.rentstate;
 
+import com.example.rentstate.properties.domain.model.entities.Property;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class RentstateApplicationTests {
 
     @Test
-    void contextLoads() {
-    }
+    void whenPropertyIsPostedAndNotAvailable_thenCannotBeRented() {
+        // Arrange
+        Property property = new Property();
+        property.setPostedStatus(true);
+        property.setAvailableStatus(false);
 
+        // Act
+        boolean isPosted = property.getIsPosted();
+        boolean isAvailable = property.getAvailable();
+        property.checkRentStatus(isPosted, isAvailable);
+
+        // Assert
+        assertFalse(isAvailable, "The property cannot be rented because someone else has rented it.");
+    }
 }
